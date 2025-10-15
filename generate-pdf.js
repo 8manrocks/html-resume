28f1aa054a15a7c7f1fc0cddf92281728017560c
8manrocks <8manrocks@gmail.com>
2025-10-15 14:53:30 +0530

    changes

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,23 +1,43 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
-
+const margins = {
+  top: '15mm',
+  right: '15mm',
+  bottom: '15mm',
+  left: '15mm'
+};
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
+  await page.goto(`file:${path.resolve(__dirname, "index.html")}`, {
+    waitUntil: "networkidle0",
+  });
+  await page.pdf({
+    path: "index.pdf",
+    format: "A4",
+    margin: margins,
+    printBackground: true,
+  });
+
+  await page.goto(`file:${path.resolve(__dirname, "man.html")}`, {
+    waitUntil: "networkidle0",
+  });
+  await page.pdf({
+    path: "man.pdf",
+    format: "A4",
+    margin: margins,
+    printBackground: true,
+  });
 
   await page.goto(`file:${path.resolve(__dirname, "fe.html")}`, {
     waitUntil: "networkidle0",
   });
   await page.pdf({
-    path: "resume.pdf",
+    path: "fe.pdf",
     format: "A4",
-     margin: {
-      top: '0.4in',
-      right: '0.4in',
-      bottom: '0.4in',
-      left: '0.4in'
-    },
+    margin: margins,
     printBackground: true,
   });
 
